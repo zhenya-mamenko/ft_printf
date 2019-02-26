9a311004091dd3cafc51e0affb31ff5bf64f6691
Evgenii Mamenko <mamenko@gmail.com>
2019-02-25 17:25:09 -0800

    norm

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,13 @@
 /*   By: emamenko <emamenko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/25 10:26:32 by emamenko          #+#    #+#             */
-/*   Updated: 2019/02/25 10:34:47 by emamenko         ###   ########.fr       */
+/*   Updated: 2019/02/25 17:24:33 by emamenko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static
 
 int		ft_printf(const char *format, ...)
 {
--- ft_printf.h
+++ ft_printf.h
@@ -6,12 +6,12 @@
 /*   By: emamenko <emamenko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/25 10:24:06 by emamenko          #+#    #+#             */
-/*   Updated: 2019/02/25 10:33:53 by emamenko         ###   ########.fr       */
+/*   Updated: 2019/02/25 17:24:24 by emamenko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-# define  FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include <unistd.h>
 # include <stdlib.h>
